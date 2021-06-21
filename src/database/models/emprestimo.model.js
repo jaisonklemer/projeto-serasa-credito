ded4547b46fdb701c5b9eeb52ec9d2d1bab6c868
Jaison <klemerjaison@gmail.com>
2021-06-21 16:35:52 -0300

    emprestimo model

---
+++ src/database/models/emprestimo.model.js
@@ -0,0 +1,23 @@
+var mongoose = require("mongoose");
+const uniqid = require("uniqid");
+
+const emprestimoSchema = new mongoose.Schema(
+  {
+    id: String,
+    idCredito: String,
+    user: String,
+    valorEmprestimo: Number,
+    valorParcela: Number,
+    qtdParcelas: Number,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+emprestimoSchema.pre("save", async function (next) {
+  this.id = uniqid();
+  next();
+});
+
+module.exports = mongoose.model("Emprestimo", emprestimoSchema);
